9bf32f4811a8c38209c0e5f3c84dea9c9dae036d
Xavier Delylle <kamixdell@gmail.com>
2021-03-19 15:34:15 +0100

    parcel

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// dans ce fichier js "point d'entrée"
+
+// on peut aller chercher des dépendances de node_modules
+import 'reset-css';
+
+// on importe notre style custom (SCSS)
+import '../scss/main.scss';
+let picture = document.querySelector("body > main > div");
+let before = document.querySelector("body > main > div > div.picture--before");
+let after = document.querySelector("body > main > div > div.picture--after");
+picture.addEventListener("mouseover", handlePicture());
+function handlePicture(evt){
+    before.style.display = "none";
+    after.style.display = "block";
+}
