5280ba51df30321dcad5a5ff1c7d70969b5b6093
Xavier Delylle <kamixdell@gmail.com>
2021-03-22 17:52:29 +0100

    remove reset.css

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,3 @@ import 'reset-css';
 
 // on importe notre style custom (SCSS)
 import '../scss/main.scss';
-
-
-// let picture = document.querySelector("body > main > div");
-// let before = document.querySelector("body > main > div > div.picture--before");
-// let after = document.querySelector("body > main > div > div.picture--after");
-// picture.addEventListener("mouseover", handlePicture());
-// function handlePicture(evt){
-//     before.style.display = "none";
-//     after.style.display = "block";
-// }
