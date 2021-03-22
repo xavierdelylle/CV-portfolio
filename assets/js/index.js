0b46e8e7fc0c2bae8763b050e3a32745feeb22da
Xavier Delylle <kamixdell@gmail.com>
2021-03-22 17:16:25 +0100

    desktop ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,13 @@ import 'reset-css';
 
 // on importe notre style custom (SCSS)
 import '../scss/main.scss';
-let picture = document.querySelector("body > main > div");
-let before = document.querySelector("body > main > div > div.picture--before");
-let after = document.querySelector("body > main > div > div.picture--after");
-picture.addEventListener("mouseover", handlePicture());
-function handlePicture(evt){
-    before.style.display = "none";
-    after.style.display = "block";
-}
+
+
+// let picture = document.querySelector("body > main > div");
+// let before = document.querySelector("body > main > div > div.picture--before");
+// let after = document.querySelector("body > main > div > div.picture--after");
+// picture.addEventListener("mouseover", handlePicture());
+// function handlePicture(evt){
+//     before.style.display = "none";
+//     after.style.display = "block";
+// }
