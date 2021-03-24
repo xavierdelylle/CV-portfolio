832def5bf4b4039998b991b2c0e6ecf983ba117d
Xavier Delylle <kamixdell@gmail.com>
2021-03-24 11:37:58 +0100

    production

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// dans ce fichier js "point d'entrée"
+
+// on peut aller chercher des dépendances de node_modules
+import 'reset-css';
+
+// on importe notre style custom (SCSS)
+import '../scss/main.scss';
