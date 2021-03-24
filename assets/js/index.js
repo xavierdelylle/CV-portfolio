a62f886418e0f2010459930dfd74bdb16bcaacdc
Xavier Delylle <kamixdell@gmail.com>
2021-03-24 12:30:34 +0100

    move from scss to css

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-// dans ce fichier js "point d'entrée"
-
-// on peut aller chercher des dépendances de node_modules
-import 'reset-css';
-
-// on importe notre style custom (SCSS)
-import '../scss/main.scss';
